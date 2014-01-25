4972307c515f42f8b51a7683049d29932f3a0e02
patrickpowns <patrickpowns@gmail.com>
2014-01-25 13:53:18 -0500

    dis collision is hard

--- src/physics.py
+++ src/physics.py
@@ -18,69 +18,12 @@ def resolve_collision(A, B, time):
     # for this function I make one of the objects the frame of reference
     # which means my calculations are much simplified
     
-    # find the time until a possible collision, using the x-axis
-    d_x = (A.displacement[0] - B.displacement[0]) - A.radius - B.radius
-    #h2fix: instead of just saying "A.r - B.r", use sinusoidal functions to find how much radius to use
-    print(d_x)
-    v_x = A.velocity[0] - B.velocity[0]
-    a_x = A.acceleration[0] - B.acceleration[0]
-    # kinematics equation here:
-    # d = vt + 0.5at^2, rearrange into a quadratic:
-    # 0 = (0.5a)t^2 + (v)t - d, solve for t using quadratic formula:
-    try:
-        t_x = (-v_x + m/s*sqrt((v_x**2 + 2*a_x*d_x).asNumber(m*m/s/s)))/a_x
-    except FloatingPointError:
-        # resort to simple t = v/d
-        try:
-            t_x = d_x / v_x
-        except FloatingPointError:
-            return
-    #print("neg =", (-v_x - sqrt(v_x**2 + 2*a_x*d_x))/a_x)
-    #print("pos =", (-v_x + sqrt(v_x**2 + 2*a_x*d_x))/a_x)
+    # this code finds when the the two entities will collide. See
+    # http://www.gvu.gatech.edu/people/official/jarek/graphics/material/collisionsDeshpandeKharsikarPrabhu.pdf
+    # for how I got the algorithm
     
+    a = (A.velocity * B.velocity)**2
     
-    # find the time until a possible collision, using the y-axis
-    d_y = (A.displacement[1] - B.displacement[1]) - A.radius - B.radius
-    print("d_y",d_y)
-    v_y = A.velocity[1] - B.velocity[1]
-    a_y = A.acceleration[1] - B.acceleration[1]
-    # kinematics equation here:
-    # d = vt + 0.5at^2, rearrange into a quadratic:
-    # 0 = (0.5a)t^2 + (v)t - d, solve for t using quadratic formula:
-    
-    try:
-        t_y = (-v_y + m/s * sqrt((v_y**2 + 2*a_y*d_y).asNumber(m*m/s/s)))/a_y
-    except FloatingPointError:
-        # resort to simple t = v/d
-        try:
-            t_y = d_y / v_y
-        except FloatingPointError:
-            return
-    #print("neg =", (-v_y - sqrt(v_y**2 + 2*a_y*d_y))/a_y)
-    #print("pos =", (-v_y + sqrt(v_y**2 + 2*a_y*d_y))/a_y)
-    
-    
-    # if there is a collision this frame, both t_x and t_y will be less than
-    # the time that will elapse this frame
-    #print(t_x,t_y)
-    #print("HELP I NEED AN ADULT")
-    
-    if not scipy.isfinite(t_x.asNumber(s)) or \
-       not scipy.isfinite(t_y.asNumber(s)):
-        return
-    
-    #if t_x < time and t_x > 0:
-        #print(t_x)
-    #if t_y < time and t_y > 0:
-        #print(t_y)
-
-    if t_x < t_y:
-        t_to_impact = t_x
-    else:
-        t_to_impact = t_y
-    
-    if t_to_impact > time or t_to_impact < 0 * s:
-        return
     
     # at this point, we know there is a collision
     print("Collision:", A.name, "and", B.name)
@@ -92,6 +35,7 @@ def resolve_collision(A, B, time):
     print("Collision:", A.name, "and", B.name)
     print("Collision:", A.name, "and", B.name)
     print("Collision:", A.name, "and", B.name)
+    t_to_impact = t_neg
 
     # move until the point of impact
     A.move(t_to_impact);
